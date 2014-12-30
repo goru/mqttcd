e062ad9d06802313e5e6f3af47ae81a2b627b7a4
lostman-github <lostman.worlds.end+github@gmail.com>
2014-12-31 03:09:41 +0900

    divide wrapper

--- mqttcd.h
+++ mqttcd.h
@@ -8,19 +8,10 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 
-#include "MQTTPacket.h"
-#include "transport.h"
-
 #include "mqttcd_type.h"
 #include "mqttcd_logger.h"
 #include "mqttcd_arg.h"
+#include "mqttcd_wrapper.h"
 
 static int MQTTCD_INTERRUPTED_SIGNAL = 0;
 void signal_handler(int signum);
-
-int mqtt_connect(mqttcd_context_t* context);
-int mqtt_initialize_connection(mqttcd_context_t* context);
-int mqtt_read_publish(mqttcd_context_t* context, char** payload);
-int mqtt_send_ping(mqttcd_context_t* context);
-int mqtt_finalize_connection(mqttcd_context_t* context);
-void mqtt_disconnect(mqttcd_context_t* context);
---
+++ mqttcd_wrapper.h
@@ -0,0 +1,23 @@
+/* vim: set et fenc=utf-8 ff=unix ts=4 sw=4 sts=0 : */
+
+#ifndef mqttcd_wrapper_h
+#define mqttcd_wrapper_h
+
+#include <stdlib.h>
+#include <string.h>
+
+#include "MQTTPacket.h"
+#include "transport.h"
+
+#include "mqttcd_type.h"
+#include "mqttcd_logger.h"
+
+int mqtt_connect(mqttcd_context_t* context);
+int mqtt_initialize_connection(mqttcd_context_t* context);
+int mqtt_read_publish(mqttcd_context_t* context, char** payload);
+int mqtt_send_ping(mqttcd_context_t* context);
+int mqtt_finalize_connection(mqttcd_context_t* context);
+void mqtt_disconnect(mqttcd_context_t* context);
+
+#endif
+
