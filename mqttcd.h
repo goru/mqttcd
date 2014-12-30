6710cc3f3b2b518e60a6bca706b53c89844ebb53
lostman-github <lostman.worlds.end+github@gmail.com>
2014-12-31 02:47:17 +0900

    divide types

--- mqttcd.h
+++ mqttcd.h
@@ -13,49 +13,7 @@
 #include "MQTTPacket.h"
 #include "transport.h"
 
-#define BUFFER_LENGTH 4096
-
-enum mqttcd_status {
-    MQTTCD_SUCCEEDED,
-    MQTTCD_FORK_FAILED,
-    MQTTCD_SETUP_SIGNAL_FAILED,
-    MQTTCD_PARSE_ARG_FAILED,
-    MQTTCD_OPEN_FAILED,
-    MQTTCD_SERIALIZE_FAILED,
-    MQTTCD_DESERIALIZE_FAILED,
-    MQTTCD_SEND_PACKET_FAILED,
-    MQTTCD_READ_PACKET_TIMEOUT,
-    MQTTCD_PACKET_TYPE_MISMATCHED
-};
-
-typedef struct _mqttcd_raw_option {
-    char* host;
-    char* port;
-    char* version;
-    char* client_id;
-    char* username;
-    char* password;
-    char* topic;
-    char* daemonize;
-} mqttcd_raw_option_t;
-
-typedef struct _mqttcd_option {
-    char* host;
-    int port;
-    int version;
-    char* client_id;
-    char* username;
-    char* password;
-    char* topic;
-    int daemonize;
-} mqttcd_option_t;
-
-typedef struct _mqttcd_context {
-    mqttcd_raw_option_t raw_option;
-    mqttcd_option_t option;
-    int mqtt_socket;
-    FILE* logger;
-} mqttcd_context_t;
+#include "mqttcd_type.h"
 
 int logger_open(mqttcd_context_t* context);
 int logger_close(mqttcd_context_t* context);
---
+++ mqttcd_type.h
@@ -0,0 +1,53 @@
+/* vim: set et fenc=utf-8 ff=unix ts=4 sw=4 sts=0 : */
+
+#ifndef mqttcd_type_h
+#define mqttcd_type_h
+
+#include <stdio.h>
+
+#define BUFFER_LENGTH 4096
+
+enum mqttcd_status {
+    MQTTCD_SUCCEEDED,
+    MQTTCD_FORK_FAILED,
+    MQTTCD_SETUP_SIGNAL_FAILED,
+    MQTTCD_PARSE_ARG_FAILED,
+    MQTTCD_OPEN_FAILED,
+    MQTTCD_SERIALIZE_FAILED,
+    MQTTCD_DESERIALIZE_FAILED,
+    MQTTCD_SEND_PACKET_FAILED,
+    MQTTCD_READ_PACKET_TIMEOUT,
+    MQTTCD_PACKET_TYPE_MISMATCHED
+};
+
+typedef struct _mqttcd_raw_option {
+    char* host;
+    char* port;
+    char* version;
+    char* client_id;
+    char* username;
+    char* password;
+    char* topic;
+    char* daemonize;
+} mqttcd_raw_option_t;
+
+typedef struct _mqttcd_option {
+    char* host;
+    int port;
+    int version;
+    char* client_id;
+    char* username;
+    char* password;
+    char* topic;
+    int daemonize;
+} mqttcd_option_t;
+
+typedef struct _mqttcd_context {
+    mqttcd_raw_option_t raw_option;
+    mqttcd_option_t option;
+    int mqtt_socket;
+    FILE* logger;
+} mqttcd_context_t;
+
+#endif
+
