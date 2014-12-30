185871a9c4990aa7ba4b3d6ac1ef3a96d6c59240
lostman-github <lostman.worlds.end+github@gmail.com>
2014-12-31 03:13:59 +0900

    cleanup includes

--- mqttcd.h
+++ mqttcd.h
@@ -1,12 +1,11 @@
 /* vim: set et fenc=utf-8 ff=unix ts=4 sw=4 sts=0 : */
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <signal.h>
 #include <unistd.h>
+#include <stdio.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <signal.h>
+#include <stdlib.h>
 
 #include "mqttcd_type.h"
 #include "mqttcd_logger.h"
